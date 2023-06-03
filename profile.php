10a3f5097d0a587e0e3b9305612058411dc86fef
sabin <burlacusabin04@yahoo.com>
2023-06-03 21:57:50 +0300

    modified files

--- profile.php
+++ profile.php
@@ -8,35 +8,43 @@
 ?>
 
 <section class="profile">
-    <p class="username">
-        <?php
-            echo $_SESSION["useruid"];
-        ?>
-    </p>
-
-    <div class="info">
-        <h3>
+    <div class="left-container">
+        <p class="username">
             <?php
-                $profileInfo->fetchTitle($_SESSION["userid"]);
-            ?>
-        </h3>
-        <p>
-            <?php
-                $profileInfo->fetchText($_SESSION["userid"]);
+                echo $_SESSION["useruid"];
             ?>
         </p>
     </div>
-
-    <div class="about">
-        <h3>ABOUT</h3>
-        <p>
-            <?php
-                $profileInfo->fetchAbout($_SESSION["userid"]);
-            ?>
-        </p>
+    <div class="right-container">
+        <div class="info">
+            <h3>
+                <?php
+                    $profileInfo->fetchTitle($_SESSION["userid"]);
+                ?>
+            </h3>
+            <p>
+                <?php
+                    $profileInfo->fetchText($_SESSION["userid"]);
+                ?>
+            </p>
+        </div>
     </div>
-    <a href="profilesettings.php" class="settings">PROFILE SETTINGS</a>
-</section>
+    <div class="right-container">
+        <div class="about">
+            <h3>ABOUT</h3>
+            <p>
+                <?php
+                    $profileInfo->fetchAbout($_SESSION["userid"]);
+                ?>
+            </p>
+        </div>
+        <a href="profilesettings.php" class="settings">PROFILE SETTINGS</a>
+    </div>
+    </section>
+
+
+
+
 
 
 </body>
