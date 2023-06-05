c59ec0799bedbb3ff23007e18efadf3697904a31
sabin <burlacusabin04@yahoo.com>
2023-06-05 22:31:00 +0300

    new fles

--- profile.php
+++ profile.php
@@ -7,11 +7,30 @@
     $profileInfo = new ProfileInfoView();
 ?>
 
-        <p>
-            <?php
-                echo $_SESSION["useruid"];
-            ?>
-        </p>
+<section class="profile">
+    <div class="left-side">
+        <div class="profile-intro">
+            <img src="images/profile-pic.jpg" alt="">
+            <p>
+                <?php
+                    echo $_SESSION["useruid"];
+                ?>
+            </p>
+            <a href="profilesettings.php" class="settings">PROFILE SETTINGS</a>
+        </div>
+        <div class="about">
+            <h3>ABOUT</h3>
+            <p>
+                <?php
+                    $profileInfo->fetchAbout($_SESSION["userid"]);
+                ?>
+            </p>
+            <h3>SUBSCRIBERS</h3>
+        </div>
+    </div>
+
+    <div class="right-side">
+        <div class="introduction">
             <h3>
                 <?php
                     $profileInfo->fetchTitle($_SESSION["userid"]);
@@ -22,21 +41,19 @@
                     $profileInfo->fetchText($_SESSION["userid"]);
                 ?>
             </p>
+        </div>
+        <div class="videos">
+            <h3>VIDEOS</h3>
+            <p>YOU DON'T HAVE UPLOADED VIDEOS</p>
+        </div>
+    </div>
+</section>
+      
+
+            
 
 
-            <h3>ABOUT</h3>
-            <p>
-                <?php
-                    $profileInfo->fetchAbout($_SESSION["userid"]);
-                ?>
-            </p>
-
-        <a href="profilesettings.php" class="settings">PROFILE SETTINGS</a>
-
-
-
-
-
+            
 
 
 </body>
